4a0bd1351900c09294bdbc6a6a5a8d5fdb6960ee
Nelson Malbone <nhmalbone@icloud.com>
2025-03-29 22:55:14 -0400

    03-29 created the window with tkinter

--- main.py
+++ main.py
@@ -2,3 +2,10 @@
 # Tkinter Temperature Convertor
 import tkinter as tk
 
+def main():
+    main_window = tk.Tk()
+    main_window.title("Temperature Gui Converter")
+
+    main_window.mainloop()
+
+main()
