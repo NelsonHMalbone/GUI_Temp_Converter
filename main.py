b8a74928a52f1b7c269864f13a99d3c721b12fb8
Nelson Malbone <nhmalbone@icloud.com>
2025-03-29 23:00:00 -0400

    03-29 notes that i forget

--- main.py
+++ main.py
@@ -3,9 +3,13 @@
 import tkinter as tk
 
 def main():
+    # this is the main window system
     main_window = tk.Tk()
     main_window.title("Temperature Gui Converter")
+    main_window.geometry("400x400")
 
+
+    # to run the program
     main_window.mainloop()
 
 main()
