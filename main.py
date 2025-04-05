1dec32cc3760c62369ac145118889fb76110698c
Nelson Malbone <nhmalbone@icloud.com>
2025-04-05 02:59:36 -0400

    03-29 working on the functions for the converter formulas

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 # 03-29-2025
 # Tkinter Temperature Convertor
 import tkinter as tk
+from tkinter import ttk
 
 def main():
+    # functions for the formulas of the temperature Convertors
+    def Celsius_to_Fahrenheit():
+        pass
+
+    def Fahrenheit_to_Celsius():
+         pass
+
+    def Celsius_to_Kelvin():
+         pass
+
+    def Kelvin_to_Celsius():
+        pass
+
+
+
     # this is the main window system
     main_window = tk.Tk()
     main_window.title("Temperature Gui Converter")
     main_window.geometry("400x400")
 
-
-
-    # to run the program
     main_window.mainloop()
 
 main()
