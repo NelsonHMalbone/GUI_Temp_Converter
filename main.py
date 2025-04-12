a8fd2125199d4910cc6e21b8b8a6a0527c596a87
Nelson Malbone <nhmalbone@icloud.com>
2025-04-12 19:26:16 -0400

    04-12 configure the main window and set the geometry

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import tkinter as tk
+from tkinter import ttk
 
 def main():
+    main_window = tk.Tk()
+    main_window.geometry('300x200')
+    main_window.title("Temperature Unit Conversion")
+    main_window.mainloop()
+
 
 if __name__ == "__main__":
     main()
