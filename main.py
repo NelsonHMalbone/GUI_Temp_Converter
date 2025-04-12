8c61ba3de82d502edd458763675b525d70cfbbd0
Nelson Malbone <nhmalbone@icloud.com>
2025-04-12 19:45:46 -0400

    04-12 user input and label section

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ def main():
     main_window = tk.Tk()
     main_window.geometry('300x200')
     main_window.title("Temperature Unit Conversion")
+
+    # where the user inputs the Temperature
+    user_input_label = tk.Label(main_window, text="Value to Convert: ")
+    user_input_label.pack()
+    user_input_entry = tk.Entry(main_window)
+    user_input_entry.pack()
+
+
     main_window.mainloop()
 
 
