d5b35ca3f642549c69c6fae07934e91db08dac7a
Nelson Malbone <nhmalbone@icloud.com>
2025-04-12 22:55:12 -0400

    04-12 combobox option and cleaned up some sections of the code

--- main.py
+++ main.py
@@ -6,10 +6,17 @@ def main():
     main_window.geometry('300x200')
     main_window.title("Temperature Unit Conversion")
 
+    # options for user to choose from for conversation
+    options = ['Celsius to Fahrenheit', 'Fahrenheit to Celsius']
+    combo_box_label = tk.Label(main_window, text="Choose a Conversion Option")
+    combo_box_options = ttk.Combobox(main_window, values=options)
+    combo_box_label.pack()
+    combo_box_options.pack()
+
     # where the user inputs the Temperature
     user_input_label = tk.Label(main_window, text="Value to Convert: ")
-    user_input_label.pack()
     user_input_entry = tk.Entry(main_window)
+    user_input_label.pack()
     user_input_entry.pack()
 
 
